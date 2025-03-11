8db8ec5d945ae875e4125fe325c2f59ddeb1eee6
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-03-11 10:44:23 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
---
+++ hosts/default/configuration.nix
@@ -0,0 +1,246 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  #  users.users.ray = {
+  #   isNormalUser = true;
+  #    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #    packages = with pkgs; [
+  #      tree
+  #    ];
+  #  };
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    services.displayManager.sddm = {
+      enable = true;
+      theme = "breeze";
+      wayland = {
+      	enable = true;
+      };
+      autoLogin = {
+      	enable = true;
+	user = "ray";
+      };
+    };
+    services.displayManager.defaultSession = "hyprland";
+
+    # programs.firefox.enable = true;
+    programs.zsh.enable = true;
+    programs.thunar.enable = true;
+
+    programs.thunar.plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+
+    programs.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+
+    environment.sessionVariables = {
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NIXOS_OZONE_WL = "1";
+    };
+
+    hardware = {
+      opengl.enable = true;
+    };
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      zsh
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      neovim
+      wget
+      kitty
+      ghostty
+      hyprland
+      waybar
+      rofi-wayland
+      fzf
+      ripgrep
+      fd
+      tmux
+      cargo
+      rustc
+      nix
+      git
+      lazygit
+      luarocks
+      zoxide
+      eza
+      unzip
+      gcc
+      clang
+      clang-tools
+      hyprpaper
+      yazi
+      go
+      nodejs
+      python3
+      zig
+      imagemagick
+      ghostscript
+      pandoc
+      ffmpeg
+      sqlite
+      pywal
+      delta
+      bat
+      wl-clipboard
+      xclip
+      copyq
+      jq
+      bat
+      librewolf
+      inputs.zen-browser.packages.${pkgs.system}.default
+    ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.code-new-roman
+  	noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    fira-code
+    jetbrains-mono
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  services.flatpak.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # Keyboard layout
+  services.xserver.xkb.layout = "us";
+  
+  # Users
+  users.users.ray = {
+    isNormalUser = true;
+    description = "Ray";
+    extraGroups = [ "wheel" ]; # Sudo access
+    shell = pkgs.zsh;
+    home = "/home/ray";
+    hashedPassword = "$6$DedRov8cUooYCjBn$lj13pps1hQXCaLhG5Kt0ql6JmJXMtNAyv1xbq8GncAEFrmNXIsGzk0r2zkHU6pmgoNuDY1hYq7lbsREK8r4W10";
+  };
+
+  # Bootloader
+  boot.loader.grub.device = "/dev/sda";
+
+  home-manager = {
+      extraSpecialArgs = { inherit inputs; };
+      users = {
+          "ray" = import ./home.nix;
+      };
+  };
+}
