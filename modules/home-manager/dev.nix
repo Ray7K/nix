6089890d3d8e854c2635080fe48864b9c3f43f21
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-11-01 19:56:28 +0530

    remove redundant clang wrapper for darwin

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -1,62 +1,66 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    # toolchains and compilers
-    clang
-    clang-tools
-    llvmPackages.llvm
-    rustup
-    zig
-    go
+  home.packages =
+    with pkgs;
+    [
+      # toolchains and compilers
+      rustup
+      zig
+      go
 
-    # runtimes and package managers
-    python3
-    nodejs
-    bun
-    pnpm
-    luarocks
+      # runtimes and package managers
+      python3
+      nodejs
+      bun
+      pnpm
+      luarocks
 
-    # build helpers
-    gnumake
-    ninja
-    bear
-    cmake
-    meson
+      # build helpers
+      gnumake
+      ninja
+      bear
+      cmake
+      meson
 
-    # lsps
-    vscode-langservers-extracted
-    typescript-language-server
-    emmet-language-server
-    basedpyright
-    nixd
-    gopls
-    zls
-    lua-language-server
-    yaml-language-server
-    taplo
+      # lsps
+      vscode-langservers-extracted
+      typescript-language-server
+      emmet-language-server
+      basedpyright
+      nixd
+      gopls
+      zls
+      lua-language-server
+      yaml-language-server
+      taplo
 
-    # formatters
-    prettierd
-    black
-    isort
-    nixfmt-rfc-style
-    stylua
+      # formatters
+      prettierd
+      black
+      isort
+      nixfmt-rfc-style
+      stylua
 
-    # linters
-    eslint_d
+      # linters
+      eslint_d
 
-    # debuggers
-    gdb
-    lldb
-    gef
-    delve
-    vscode-extensions.vadimcn.vscode-lldb
+      # debuggers
+      lldb
+      gdb
+      gef
+      delve
+      vscode-extensions.vadimcn.vscode-lldb
 
-    # other tools
-    ghidra
-    gemini-cli
-  ];
+      # other tools
+      ghidra
+      gemini-cli
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      clang
+      clang-tools
+      llvmPackages.llvm
+    ];
 
   home.sessionPath = [
     "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter"
